a699e1a57ab2ac9f7337460fa9c9c99b072167d0
Gerald Nyeo <geraldnyeo@gmail.com>
2025-07-27 22:42:40 +0800

    Updater test

--- src-tauri/src/agenda.rs
+++ src-tauri/src/agenda.rs
@@ -1,10 +1,10 @@
 // Imports
-use std::path::PathBuf;
-use std::collections::{HashMap, HashSet};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use tauri::{Manager, AppHandle, Wry};
-use tauri_plugin_store::{StoreExt};
+use std::collections::{HashMap, HashSet};
+use std::path::PathBuf;
+use tauri::{AppHandle, Manager, Wry};
+use tauri_plugin_store::StoreExt;
 
 // Task struct
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
@@ -19,7 +19,7 @@ pub struct Task {
 
     // subtask data
     pub parent: Option<String>,
-    pub subtasks: Vec<String>
+    pub subtasks: Vec<String>,
 }
 
 // Configuration
@@ -30,7 +30,9 @@ const TASK_PREFIX: &str = "task_";
 // Get path to store file
 pub fn get_store_path(app_handle: &AppHandle<Wry>, store_file: &str) -> Result<PathBuf, String> {
     // Get the path directory as a PathBuf
-    let app_data_dir = app_handle.path().app_data_dir()
+    let app_data_dir = app_handle
+        .path()
+        .app_data_dir()
         .map_err(|e| e.to_string())?;
 
     // Create directory if it doesn't already exist
@@ -50,8 +52,7 @@ pub async fn read_tasks(app_handle: AppHandle<Wry>) -> Result<Vec<Task>, String>
 
     // Get list of task_ids from store
     let task_ids: Vec<String> = if let Some(value) = store.get(ALL_TASK_IDS_KEY) {
-        serde_json::from_value(value)
-            .map_err(|e| format!("Failed to deserialize tasks: {}", e))?
+        serde_json::from_value(value).map_err(|e| format!("Failed to deserialize tasks: {}", e))?
     } else {
         Vec::new()
     };
@@ -60,11 +61,14 @@ pub async fn read_tasks(app_handle: AppHandle<Wry>) -> Result<Vec<Task>, String>
     let mut tasks: Vec<Task> = Vec::with_capacity(task_ids.len());
     for id in task_ids {
         let task_key = format!("{}{}", TASK_PREFIX, id);
-        
+
         if let Some(value) = store.get(&task_key) {
             match serde_json::from_value::<Task>(value) {
                 Ok(task) => tasks.push(task),
-                Err(e) => eprintln!("Warning: Failed to deserialize task with key {}: {}", task_key, e),
+                Err(e) => eprintln!(
+                    "Warning: Failed to deserialize task with key {}: {}",
+                    task_key, e
+                ),
             }
         } else {
             eprintln!("Warning: Task data missing for ID: {}", id);
@@ -77,15 +81,17 @@ pub async fn read_tasks(app_handle: AppHandle<Wry>) -> Result<Vec<Task>, String>
 
 // Write Tasks
 #[tauri::command]
-pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) -> Result<(), String> {
+pub async fn sync_tasks(
+    app_handle: AppHandle<Wry>,
+    updated_tasks: Vec<Task>,
+) -> Result<(), String> {
     // Get instance of store
     let store_path = get_store_path(&app_handle, TASK_STORE_FILE_NAME)?;
     let store = app_handle.store(store_path).map_err(|e| e.to_string())?;
 
     // Get (outdated) list of task_ids from store
     let outdated_task_ids: Vec<String> = if let Some(value) = store.get(ALL_TASK_IDS_KEY) {
-        serde_json::from_value(value)
-            .map_err(|e| e.to_string())?
+        serde_json::from_value(value).map_err(|e| e.to_string())?
     } else {
         Vec::new()
     };
@@ -94,13 +100,16 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
     let mut outdated_tasks: HashMap<String, Task> = HashMap::with_capacity(outdated_task_ids.len());
     for id in &outdated_task_ids {
         let task_key = format!("{}{}", TASK_PREFIX, id);
-        
+
         if let Some(value) = store.get(&task_key) {
             match serde_json::from_value::<Task>(value) {
                 Ok(task) => {
                     outdated_tasks.insert(task.id.clone(), task);
-                },
-                Err(e) => eprintln!("Warning: Failed to deserialize task with key {}: {}", task_key, e),
+                }
+                Err(e) => eprintln!(
+                    "Warning: Failed to deserialize task with key {}: {}",
+                    task_key, e
+                ),
             }
         }
     }
@@ -108,7 +117,10 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
     // Get (updated) list of task_ids
     let mut sorted_updated_tasks = updated_tasks.clone();
     sorted_updated_tasks.sort_by(|t1, t2| t1.index.cmp(&t2.index));
-    let updated_task_ids:Vec<String> = sorted_updated_tasks.iter().map(|task| task.id.clone()).collect();
+    let updated_task_ids: Vec<String> = sorted_updated_tasks
+        .iter()
+        .map(|task| task.id.clone())
+        .collect();
 
     // 1: Process additions and updates
     for task in updated_tasks {
@@ -116,13 +128,15 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
         let updated_task = serde_json::to_value(&task)
             .map_err(|e| format!("Failed to serialize task {}: {}", task.id, e))?;
 
-        if let Some(outdated_task) = outdated_tasks.get(&task.id) { // if task already exists
+        if let Some(outdated_task) = outdated_tasks.get(&task.id) {
+            // if task already exists
             // check if any changes; update if necessary
             if *outdated_task != task {
                 store.set(&task_key, updated_task);
                 println!("Updated task: {}", task.id);
             }
-        } else { // if task does not exist yet; add
+        } else {
+            // if task does not exist yet; add
             store.set(&task_key, updated_task);
             println!("Added task: {}", task.id);
         }
@@ -130,7 +144,8 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
 
     // 2: Process deletions
     let outdated_task_ids_set: HashSet<String> = outdated_task_ids.into_iter().collect();
-    let updated_task_ids_set: HashSet<String> = updated_task_ids.iter().map(|id| id.clone()).collect();
+    let updated_task_ids_set: HashSet<String> =
+        updated_task_ids.iter().map(|id| id.clone()).collect();
     for id in outdated_task_ids_set.difference(&updated_task_ids_set) {
         let task_key = format!("{}{}", TASK_PREFIX, id);
         store.delete(&task_key);
@@ -138,7 +153,12 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
     }
 
     // Update task_id master list
-    let updated_task_ids_serde: Value = Value::Array(updated_task_ids.iter().map(|id| Value::String(id.clone())).collect());
+    let updated_task_ids_serde: Value = Value::Array(
+        updated_task_ids
+            .iter()
+            .map(|id| Value::String(id.clone()))
+            .collect(),
+    );
     store.set(ALL_TASK_IDS_KEY, updated_task_ids_serde);
 
     // Save all updates
@@ -146,7 +166,6 @@ pub async fn sync_tasks(app_handle: AppHandle<Wry>, updated_tasks: Vec<Task>) ->
 
     Ok(())
 }
-
 
 // TODO
 // Goals
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,12 @@
+use tauri_plugin_updater::UpdaterExt;
+
 mod agenda;
 use agenda::{read_tasks, sync_tasks};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
@@ -12,10 +15,39 @@ pub fn run() {
                         .build(),
                 )?;
             }
+
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                update(handle).await.unwrap();
+            });
             Ok(())
         })
         .plugin(tauri_plugin_store::Builder::new().build())
         .invoke_handler(tauri::generate_handler![read_tasks, sync_tasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+  if let Some(update) = app.updater()?.check().await? {
+    let mut downloaded = 0;
+
+    // alternatively we could also call update.download() and update.install() separately
+    update
+      .download_and_install(
+        |chunk_length, content_length| {
+          downloaded += chunk_length;
+          println!("downloaded {downloaded} from {content_length:?}");
+        },
+        || {
+          println!("download finished");
+        },
+      )
+      .await?;
+
+    println!("update installed");
+    app.restart();
+  }
+
+  Ok(())
 }
